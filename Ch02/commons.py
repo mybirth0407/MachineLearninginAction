25b82ffc04d9305d2b3dc125057752c6cd8e9d7a
Yedarm Seong <mybirth0407@gmail.com>
2017-01-05 14:24:51 +0900

    commons.py(unzip, dir remove), ch01.md

---
+++ Ch02/commons.py
@@ -0,0 +1,20 @@
+'''
+Created on Sep 16, 2010
+unzip and remove directory method
+
+@author: Yedarm Seong <mybirth0407@gmail.com>
+'''
+
+def unzip(source_file):
+    import zipfile
+    with zipfile.ZipFile(source_file, 'r') as zf:
+        zipInfo = zf.infolist()
+
+        for member in zipInfo:
+            zf.extract(member)
+    zf.close()
+
+
+def removeDirectory(directory):
+    import shutil
+    shutil.rmtree(directory)
