7f3b96668776bd7bf897dbe22c11b69957a76943
Yedarm Seong <mybirth0407@gmail.com>
2017-01-12 15:44:25 +0900

    ch04.md and ch04 code and comments

--- Ch04/bayes.py
+++ Ch04/bayes.py
@@ -5,51 +5,65 @@ Created on Oct 19, 2010
 '''
 
 '''
-Add Jan xx, 2017
+Add Jan 10, 2017
 Convert from python 2 to python 3
 Korean Comments
 @author: Yedarm Seong <mybirth0407@gmail.com>
 '''
 from numpy import *
+from commons import *
 
 
+# 데이터 집합을 불러온다.
 def loadDataSet():
     postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                    ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
                    ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
                    ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
-                   ['mr', 'licks', 'ate', 'my', 'steak',
-                       'how', 'to', 'stop', 'him'],
+                   ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
                    ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
-    classVec = [0, 1, 0, 1, 0, 1]  # 1 is abusive, 0 not
+    # 1: 폭력적인, 0: 폭력적이지 않은
+    classVec = [0, 1, 0, 1, 0, 1]
     return postingList, classVec
 
 
+# 모든 문서에 있는 모든 유일한 단어 목록을 생성한다.
 def createVocabList(dataSet):
-    vocabSet = set([])  # create empty set
+    vocabSet = set([])
     for document in dataSet:
-        vocabSet = vocabSet | set(document)  # union of the two sets
+        # 두 집합의 합집합을 계산한다.
+        vocabSet = vocabSet | set(document)
     return list(vocabSet)
 
 
+# 주어진 문서 내 어휘 목록에 있는 단어의 존재 여부를 0-1 벡터로 반환한다.
 def setOfWords2Vec(vocabList, inputSet):
     returnVec = [0] * len(vocabList)
     for word in inputSet:
         if word in vocabList:
             returnVec[vocabList.index(word)] = 1
         else:
-            print "the word: %s is not in my Vocabulary!" % word
+            print("the word: %s is not in my Vocabulary!" % word)
     return returnVec
 
 
+# 나이브 베이즈 분류기를 훈련시킨다.
 def trainNB0(trainMatrix, trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
     pAbusive = sum(trainCategory) / float(numTrainDocs)
+
+    # ones(10) = array([1., 1., ..., 1.])
     p0Num = ones(numWords)
-    p1Num = ones(numWords)  # change to ones()
+    p1Num = ones(numWords)
+
+    # 2.0으로 변경한다.
+    # p0Denom = 0.0
+    # p1Denom = 0.0
+
     p0Denom = 2.0
-    p1Denom = 2.0  # change to 2.0
+    p1Denom = 2.0
+
     for i in range(numTrainDocs):
         if trainCategory[i] == 1:
             p1Num += trainMatrix[i]
@@ -57,13 +71,24 @@ def trainNB0(trainMatrix, trainCategory):
         else:
             p0Num += trainMatrix[i]
             p0Denom += sum(trainMatrix[i])
-    p1Vect = log(p1Num / p1Denom)  # change to log()
-    p0Vect = log(p0Num / p0Denom)  # change to log()
+
+    # log로 변경한다.
+    # p1Vect = p1Num / p1Denom
+    # p0Vect = p0Num / p0Denom
+
+    # 언더플로우를 방지하기 위해 log를 사용한다.
+    # 실제 값과 그래프상 거의 유사하기 때문에 값의 오차는 고려하지 않아도 된다.
+    p1Vect = log(p1Num / p1Denom)
+    p0Vect = log(p0Num / p0Denom)
+
+    # 각 분류 항목에 대한 조건부 확률을 반환한다.
     return p0Vect, p1Vect, pAbusive
 
 
 def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
-    p1 = sum(vec2Classify * p1Vec) + log(pClass1)  # element-wise mult
+    # element-wise mult
+    # 벡터 연산을 반복문 없이 사용할 경우 퍼포먼스가 증가한다.
+    p1 = sum(vec2Classify * p1Vec) + log(pClass1)
     p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
     if p1 > p0:
         return 1
@@ -83,27 +108,37 @@ def testingNB():
     listOPosts, listClasses = loadDataSet()
     myVocabList = createVocabList(listOPosts)
     trainMat = []
+
     for postinDoc in listOPosts:
         trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+
     p0V, p1V, pAb = trainNB0(array(trainMat), array(listClasses))
     testEntry = ['love', 'my', 'dalmation']
     thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
-    print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
+
+    print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
+
     testEntry = ['stupid', 'garbage']
     thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
-    print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
+
+    print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
 
 
-def textParse(bigString):  # input is big string, #output is word list
+# 긴 문자열(파일 내용)을 입력받아 단어 리스트를 반환한다.
+def textParse(bigString):
     import re
     listOfTokens = re.split(r'\W*', bigString)
     return [tok.lower() for tok in listOfTokens if len(tok) > 2]
 
 
+# 임의로 선택된 이메일로부터 오류율을 보여준다.
 def spamTest():
     docList = []
     classList = []
     fullText = []
+
+    unzip('email.zip')
+    # email.zip 내용 중 인코딩 관련 문제가 있어 zip 파일을 수정하였다.
     for i in range(1, 26):
         wordList = textParse(open('email/spam/%d.txt' % i).read())
         docList.append(wordList)
@@ -113,45 +148,61 @@ def spamTest():
         docList.append(wordList)
         fullText.extend(wordList)
         classList.append(0)
-    vocabList = createVocabList(docList)  # create vocabulary
-    trainingSet = range(50)
-    testSet = []  # create test set
+    removeDirectory('email')
+    # 문서 리스트로부터 단어 리스트를 생성한다.
+    vocabList = createVocabList(docList)
+    # python 2
+    # python 3에서는 range 의 deletion이 되지 않는다.
+    # trainingSet = range(50)
+    trainingSet = list(range(50))
+    testSet = []
+
     for i in range(10):
         randIndex = int(random.uniform(0, len(trainingSet)))
         testSet.append(trainingSet[randIndex])
         del(trainingSet[randIndex])
+
     trainMat = []
     trainClasses = []
-    for docIndex in trainingSet:  # train the classifier (get probs) trainNB0
+
+    # trainNB0 분류기를 훈련시킨다.
+    for docIndex in trainingSet:
         trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
         trainClasses.append(classList[docIndex])
+
     p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
     errorCount = 0
-    for docIndex in testSet:  # classify the remaining items
+
+    # 남은 아이템을 분류한다.
+    for docIndex in testSet:
         wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
         if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
             errorCount += 1
-            print "classification error", docList[docIndex]
-    print 'the error rate is: ', float(errorCount) / len(testSet)
-    # return vocabList,fullText
+            print("classification error", docList[docIndex])
+
+    print('the error rate is: ', float(errorCount) / len(testSet))
+    return vocabList,fullText
 
 
+# 자주 등장하는 30개의 단어를 반환한다.
 def calcMostFreq(vocabList, fullText):
     import operator
     freqDict = {}
     for token in vocabList:
         freqDict[token] = fullText.count(token)
-    sortedFreq = sorted(freqDict.iteritems(),
+    sortedFreq = sorted(freqDict.items(),
                         key=operator.itemgetter(1), reverse=True)
     return sortedFreq[:30]
 
 
+# feed1과 feed0에서 지역 특색에 따른 단어 사용을 도출한다.
 def localWords(feed1, feed0):
     import feedparser
     docList = []
     classList = []
     fullText = []
     minLen = min(len(feed1['entries']), len(feed0['entries']))
+
     for i in range(minLen):
         wordList = textParse(feed1['entries'][i]['summary'])
         docList.append(wordList)
@@ -161,47 +212,69 @@ def localWords(feed1, feed0):
         docList.append(wordList)
         fullText.extend(wordList)
         classList.append(0)
-    vocabList = createVocabList(docList)  # create vocabulary
-    top30Words = calcMostFreq(vocabList, fullText)  # remove top 30 words
+
+    # 문서 리스트로부터 단어 리스트를 생성한다.
+    vocabList = createVocabList(docList)
+
+    # 중복과 구조적 접속사 제거를 위해
+    # 가장 많이 등장하는 30개의 단어를 제거한다.
+    top30Words = calcMostFreq(vocabList, fullText)
     for pairW in top30Words:
         if pairW[0] in vocabList:
             vocabList.remove(pairW[0])
-    trainingSet = range(2 * minLen)
-    testSet = []  # create test set
+
+    # python 2
+    # trainingSet = range(2 * minLen)
+    trainingSet = list(range(2 * minLen))
+    testSet = []
+
     for i in range(20):
         randIndex = int(random.uniform(0, len(trainingSet)))
         testSet.append(trainingSet[randIndex])
         del(trainingSet[randIndex])
+
     trainMat = []
     trainClasses = []
-    for docIndex in trainingSet:  # train the classifier (get probs) trainNB0
+
+    # trainNB0 분류기를 훈련시킨다.
+    for docIndex in trainingSet:
         trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
         trainClasses.append(classList[docIndex])
+
     p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
     errorCount = 0
-    for docIndex in testSet:  # classify the remaining items
+
+    # 남은 아이템을 분류한다.
+    for docIndex in testSet:
         wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
         if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
             errorCount += 1
-    print 'the error rate is: ', float(errorCount) / len(testSet)
+
+    print('the error rate is: ', float(errorCount) / len(testSet))
     return vocabList, p0V, p1V
 
 
+# 등장한 대부분의 단어들을 표시한다.
 def getTopWords(ny, sf):
     import operator
     vocabList, p0V, p1V = localWords(ny, sf)
     topNY = []
     topSF = []
+
     for i in range(len(p0V)):
         if p0V[i] > -6.0:
             topSF.append((vocabList[i], p0V[i]))
         if p1V[i] > -6.0:
             topNY.append((vocabList[i], p1V[i]))
+
     sortedSF = sorted(topSF, key=lambda pair: pair[1], reverse=True)
-    print "SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**"
+    print("SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**")
+
     for item in sortedSF:
-        print item[0]
+        print(item[0])
+
     sortedNY = sorted(topNY, key=lambda pair: pair[1], reverse=True)
-    print "NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**"
+    print("NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**")
+
     for item in sortedNY:
-        print item[0]
+        print(item[0])
--- ch04-naive_bayes.md
+++ ch04-naive_bayes.md
@@ -3,3 +3,123 @@
 
 ###__4장 나이브 베이즈(Naive Bayes)__
 
+
+k-NN이나 의사결정 트리와 같은 알고리즘은 분류기로써 가끔 잘못된 예측을 하기도 했다.  
+이러한 잘못된 예측 대신, 분류기에게 확률을 요청하도록 만들 수도 있다.  
+
+많은 기계 학습 알고리즘들은 확률 이론에 기반을 두고 있다.  
+이번 장에서는 의사결정 트리의 참-거짓에 대한 부분을 확률로 다룬다.  
+같은 속성이 등장한 횟수를 데이터 집합의 전체 사례의 수로 나눠 확률을 계산한다.  
+
+이번 장에서는 확률 이론으로 데이터를 분류하는 몇 가지 방법을 알아볼 것이다.  
+간단한 확률 분류기부터 약간의 가정을 통한 나이브 베이즈(Naive Bayes) 분류기를 학습한다.  
+'나이브(Naive)' 라는 단어를 사용하는 이유는 몇 가지 간단한(Naive)한 가정을 통해 만들어진 것이기 때문이다.  
+이 장에서는 조건부 확률에 대해서도 다룰 것이다.  
+
+####4.1 베이지안 의사결정 이론으로 분류하기
+
+|| 나이브 베이즈 |
+| :---: | :---: |
+| 장점 | 소량의 데이터를 가지고 작업이 이루어지며, 여러 개의 분류 항목을 다룰 수 있다. |
+| 단점 | 입력 데이터에 따라 민감하게 작용한다. |
+| 적용 | 명목형 값 |
+
+나이브 베이즈는 베이즈 정리의 일부분이다.  
+따라서 나이브 베이즈 이전에 베이즈 정리 이론을 짚고 넘어갈 것이다.  
+
+데이터 내에 두 개의 분류 항목이 있는 데이터 집합을 가정한다.  
+분류 항목 1에 속할 확률 방정식 ![equation](https://latex.codecogs.com/gif.latex?p1%28x%2C%20y%29), 분류 항목 2에 속할 확률 방정식 ![equation](https://latex.codecogs.com/gif.latex?p2%28x%2C%20y%29)가 있다.  
+속성 (x, y)를 가지고 분류 확률을 선택할 때 다음과 같은 규칙을 따른다.  
+
+![equation](https://latex.codecogs.com/gif.latex?p1%28x%2C%20y%29%20%3E%20p2%28x%2C%20y%29)이면 분류항목 1에 속한다.  
+![equation](https://latex.codecogs.com/gif.latex?p2%28x%2C%20y%29%20%3E%20p1%28x%2C%20y%29)이면 분류항목 1에 속한다.  
+
+요약하면, 더 높은 확률을 갖는 분류 항목을 선택한다.  
+베이즈 정리 이론은 결국 높은 확률을 갖는 의사결정을 선택하는 것이다.  
+
+1000개의 데이터를 갖는 집합에 대해 의사결정을 선택해야 할 때, 다음 세 가지 방법이 있다.  
+1. k-NN을 사용하여 1000개의 거리 계산을 수행한다.  
+2. 의사결정 트리를 사용하여 데이터를 분할한다.  
+3. 각 분류 항목의 확률은 계산하고 비교한다.  
+
+의사결정 트리는 매우 성공적이지는 않았고, k-NN은 간단한 확률을 비교하는 많은 계산이 요구된다.  
+이러한 점들을 감안한다면 확률적인 비교가 가장 효과적일 수 있다.  
+
+####4.2 조건부 확률
+
+7개의 돌이 담긴 병이 있다.  
+그림 4.1와 같이 3개는 회색, 4개는 검은색의 돌이다.  
+그림 4.1
+
+![image](https://s24.postimg.org/y6vhu6zr9/image.png)
+
+이 중 임의로 하나의 돌을 꺼낸다면, 뽑은 돌이 회색일 확률은 3/7 이다.  
+이것을 ![equation](https://latex.codecogs.com/gif.latex?p%28gray%29)라고 하고, 이 값은 회색 돌의 개수를 세고, 전체 돌의 개수를 나눔으로써 계산된다.  
+
+그렇다면 돌들이 두 병에 나눠져 있다면 어떻게 될 지 생각해볼 수 있다.
+
+그림 4.2, 병 A
+
+![image](https://s23.postimg.org/wfd8hafqj/image.png)
+
+그림 4.3, 병 B
+
+![image](https://s27.postimg.org/q3u5kdy77/image.png)
+
+그림 4.2와 그림 4.3에서 ![equation](https://latex.codecogs.com/gif.latex?p%28gray%29) 나 ![equation](https://latex.codecogs.com/gif.latex?p%28black%29) 를 계산한다면 어떤 병을 선택하냐에 따라 확률이 달라질 것이다.  
+병 A에서 회색 돌을 꺼낼 확률을 ![equation](https://latex.codecogs.com/gif.latex?p%28gray%7CA%29)라고 하고 이것을 조건부 확률이라 한다.  
+![equation](https://latex.codecogs.com/gif.latex?p%28gray%7CA%29)는 2/4 이고, ![equation](https://latex.codecogs.com/gif.latex?p%28gray%7CB%29)는 2/3 이다.  
+조건부 확률의 계산 방법은 다음과 같다.  
+![equation](https://latex.codecogs.com/gif.latex?p%28gray%7CB%29%20%3D%20p%28gray%20%5Ccap%20B%29%20/%20p%28B%29)
+
+![equation](https://latex.codecogs.com/gif.latex?p%28gray%20%5Ccap%20B%29)가 2/7이 되는지 확인해보면, 병 B에 있는 회색 돌을 전체 돌의 개수로 나누는 것으로 계산할 수 있다.  
+병 B에는 7개의 돌 중 3개가 들어 있으므로 ![equation](https://latex.codecogs.com/gif.latex?p%28B%29)는 3/7이다.  
+다음으로 ![equation](https://latex.codecogs.com/gif.latex?p%28gray%7CB%29)는 앞에서 2/3이었기 때문에 우리는 원하는 값을 얻을 수 있다.  
+
+조건부 확률을 다루는 유용한 방법 중 베이즈 규칙(Bayes' rule)이 있다.  
+조건부 확률에서 베이즈 규칙은 기호 교환에 대해 설명한다.  
+만약 ![equation](https://latex.codecogs.com/gif.latex?p%28x%7Cc%29)을 알고 있는 상태에서 ![equation](https://latex.codecogs.com/gif.latex?p%28c%7Cx%29)를 알고 싶다면, 다음과 같이 찾을 수 있다.  
+
+![equation](https://latex.codecogs.com/gif.latex?p%28c%7Cx%29%20%3D%20%5Cfrac%7Bp%28x%7Cc%29%20p%28c%29%7D%7B%7D%20%7Bp%28x%29%7D)
+
+####4.3 조건부 확률로 분류하기
+조건부 확률을 알았으니 앞에서 언급한 다음 두 가지를 확장해볼 수 있다.
+![equation](https://latex.codecogs.com/gif.latex?p1%28x%2C%20y%29%20%3E%20p2%28x%2C%20y%29)이면 분류항목 1에 속한다.  
+![equation](https://latex.codecogs.com/gif.latex?p2%28x%2C%20y%29%20%3E%20p1%28x%2C%20y%29)이면 분류항목 1에 속한다.  
+
+이러한 간단한 규칙으로는 전체 내용을 설명하지는 못한다.  
+우리에게 필요한 ![equation](https://latex.codecogs.com/gif.latex?p%28c_1%7Cx%2Cy%29)와 ![equation](https://latex.codecogs.com/gif.latex?p%28c_2%7Cx%2Cy%29)을 비교해보도록 한다.  
+x, y로 정해진 한 지점이 있다면, 이 지점이 분류 항목 ![equation](https://latex.codecogs.com/gif.latex?c_1)에 속할 확률이 높은지, 분류항목 ![equation](https://latex.codecogs.com/gif.latex?c_2)에 속할 확률이 높은지에 대한 문제를 해결하기 위해 베이즈 규칙을 사용한다.  
+
+![equation](https://latex.codecogs.com/gif.latex?p%28c_i%7Cx%2Cy%29%20%3D%20%5Cfrac%7Bp%28x%2Cy%7Cc_i%29%20p%28c_i%29%7D%7Bp%28x%2Cy%29%7D)
+
+이 정의를 가지고 베이지안 분류 규칙을 정의할 수 있다.  
+
+![equation](https://latex.codecogs.com/gif.latex?p%28c_1%7Cx%2Cy%29%20%3E%20p%28c_2%7Cx%2Cy%29)라면 분류 항목 ![equation](https://latex.codecogs.com/gif.latex?c_1)에 속한다.  
+![equation](https://latex.codecogs.com/gif.latex?p%28c_2%7Cx%2Cy%29%20%3E%20p%28c_1%7Cx%2Cy%29)라면 분류 항목 ![equation](https://latex.codecogs.com/gif.latex?c_2)에 속한다.  
+
+이렇게 베이즈 규칙을 사용함으로써 알려진 확률로부터 알려지지 않은 것을 계산하고, 아이템들을 분류할 수 있다.
+
+####4.4 나이브 베이즈로 문서 분류하기
+
+기계 학습의 한 가지 중요한 응용 프로그램은 자동 문서 분류 프로그램이 있다.  
+문서 분류에 있어 전체 문서, 각각의 이메일은 하나의 사례가 되고, 이메일을 구성하고 있는 요소(단어)들은 속성이 된다.  
+속성들이 서로 독립적이라고 가정하면 데이터의 수는 하나의 단어가 다른 단어 옆에도 나타날 확률이 같다고 볼 수 있다.  
+즉, '베이컨(bacon)'이 '맛있는(delicious)'이라는 단어와 '건강에 해로운(unhealthy)'이라는 단어 옆에 나타날 확률을 같다고 가정한다.  
+또한 모든 속성에 대해 중요도도 같다고 가정한다.  
+이러한 가정들 때문에 나이브라는 단어가 붙었지만, 나이브 베이즈는 이런 결함들을 가지고도 일을 잘 수행한다.  
+
+문서의 단어 벡터 ![w](https://latex.codecogs.com/gif.latex?w)를 가지고 해당 문서가 어떤 분류 항목(![c_i](https://latex.codecogs.com/gif.latex?c_i))에 속하는지 검사하기 위해 다음과 같은 수식을 이용한다.  
+
+![equation](https://latex.codecogs.com/gif.latex?p%28c_i%7Cw%29%20%3D%20%5Cfrac%7Bp%28w%7Cc_i%29p%28c_i%29%7D%7Bp%28w%29%7D)
+
+왼쪽의 값을 구하기 위해, 오른쪽의 값을 계산한다.  
+오른쪽의 벡터 ![w](https://latex.codecogs.com/gif.latex?w)는 단어들의 벡터인데, 이를 개별적인 속성으로 펼치면 다음과 같다.
+
+![c_i](https://latex.codecogs.com/gif.latex?p%28w_0%2Cw_1%2Cw_2%2C...%2Cw_n%7Cc_i%29)
+
+위 식에서 모든 단어들이 서로 독립적이라는 가정을 통해 이에 대한 확률은 ![](https://latex.codecogs.com/gif.latex?p%28w_0%7Cc_i%29p%28w_1%7Cc_i%29p%28w_2%7Cc_i%29...p%28w_n%7Cc_i%29)로 쉽게 계산할 수 있다.  
+
+이렇듯 나이브한 가정들을 통해 문서에 대한 속성들로, 문서들을 쉽게 분류할 수 있고 이는 조금 무리한 가정이 섞여있더라도 충분히 잘 작동한다.
+
+---
